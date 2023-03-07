ba7ae3f4e75501b09b4ef3e48494ceda68f0ca36
kevinchock <71734930+kevinchock@users.noreply.github.com>
2023-03-06 21:20:07 -0300

    fede

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect, render_template, abort
+
 app = Flask(__name__)
+
+todos = ['lalal', 'chanchito feliz 2', 'lala2']
+
+@app.errorhandler(404)
+def not_found (error):
+    return render_template('404.html', error=error)
+
+@app.errorhandler(500)
+def error_server(error):
+    return render_template('500.html', error=error)
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def error_500():
+    abort(500)
 
-@app.route('/flask')
-def flask_app():
-    return "Esta es una aplicacion de Flask"
 
-@app.route('/flask-nuevo')
-def flask_app_nuevo():
-    return "Esta es una aplicacion de Flask nueva"
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+
+    return response
+
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+    context = {
+        'user_ip': user_ip,
+        'todos': todos,
+    }
+
+    return render_template('hello.html', **context)

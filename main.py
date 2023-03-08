ec6f62eba85b275238215bfed18a7d64f1718079
kevinchock <71734930+kevinchock@users.noreply.github.com>
2023-03-07 21:04:18 -0300

    Kevin

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, make_response, redirect, render_template, abort, session, url_for
+from flask import Flask, request, make_response, redirect, render_template, abort, session, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
+import unittest
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -16,6 +17,11 @@ class LoginForm(FlaskForm):
     username = StringField('Nombre de usuario', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Enviar')
+
+@app.cli.command()
+def test():
+    tests =unittest.TestLoader().discover ('tests')
+    unittest.TextTestRunner().run(tests)
 
 @app.errorhandler(404)
 def not_found (error):
@@ -47,6 +53,8 @@ def hello():
     if login_form.validate_on_submit():
         username = login_form.username.data
         session['username'] = username
+
+        flash ('Nombre de usuario registrado con exito!')
 
         return redirect(url_for('index'))
 
